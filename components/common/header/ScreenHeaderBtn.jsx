4a0708031c6cffa0fb839b288eebbf7af37bd78d
eren-the-coder <jeanpc568@gmail.com>
2025-09-10 18:16:33 +0100

    chore: remove unused style variable

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, View } from 'react-native'
 
-import styles from './screenheader.style'
 
 const ScreenHeaderBtn = () => {
   return (
