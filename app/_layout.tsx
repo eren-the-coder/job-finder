9f0cbdef624c880e865f2be2c64b6eea8e7fc1c8
eren-the-coder <jeanpc568@gmail.com>
2025-09-10 18:18:14 +0100

    feat: implement index screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,23 @@
+import { ScreenHeaderBtn } from '@/components/index';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-   <Stack />
+    <Stack>
+      <Stack.Screen 
+        name='index'
+        options={{
+          headerLeft: () => (
+            //iconUrl={icons.menu} dimensions="60%"
+            <ScreenHeaderBtn />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn />
+          ),
+          headerTitle: "",
+          headerShadowVisible: false
+        }}
+      />
+    </Stack>
   );
 }
